fc72d360e22f4f7be8dde9d03323c4e73696905a
Al-Amin Firdows <alaminfirdows@gmail.com>
2022-04-09 16:18:35 +0600

    feat: add google font style

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,13 @@ import {
 import styles from './styles/app.css'
 
 export function links() {
-    return [{ rel: 'stylesheet', href: styles }]
+    return [
+        { rel: 'stylesheet', href: styles },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Signika+Negative&display=swap',
+        },
+    ]
 }
 
 export const meta: MetaFunction = () => ({
